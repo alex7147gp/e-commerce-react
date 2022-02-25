5f6da07d7aa61849a50a6500c8ba7e9e0ff707f6
johnCC <propani333@gmail.com>
2022-02-25 15:06:10 -0500

    first app

--- src/requestMethod.jsx
+++ src/requestMethod.jsx
@@ -1,8 +1,18 @@
 import axios from 'axios';
+import {useState useEffect} from 'react';
 
 const BASE_URL = "http://localhost:5000/api/"
 
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.accessToken;
+ useEffect(()=>{
+  	getStorage = async () =>{
+       const res = await JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.accessToken;     
+  	   const TOKEN = res;
+  	   return TOKEN
+  	}
+    
+    getStorage();
+
+  },[])
 
 export const publicRequest = axios.create({
 	baseURL: BASE_URL
